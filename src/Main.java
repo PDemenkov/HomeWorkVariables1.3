db2848445bdacab15cf043bb5525f86c38fa63c7
PDemenkov <guapnik@gmail.com>
2022-09-07 20:35:01 +0300

    Задание 6

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 6");
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        System.out.println("Общий вес бойцов =" + (boxer2Weight + boxer1Weight)+("кг"));
+        System.out.println("Разнциа в весе =" + (boxer2Weight - boxer1Weight)+("кг"));
+
+    }
+}
