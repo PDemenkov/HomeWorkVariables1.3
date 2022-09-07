300c1be4c0d3ff5a8b8ec76399e14bcc515b2e9a
PDemenkov <guapnik@gmail.com>
2022-09-07 20:52:28 +0300

    Задание 7+8

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,18 @@ public class Main {
         System.out.println("Общий вес бойцов =" + (boxer2Weight + boxer1Weight)+("кг"));
         System.out.println("Разнциа в весе =" + (boxer2Weight - boxer1Weight)+("кг"));
 
+        System.out.println("Задание 7");
+        System.out.println("Разнциа в весе =" + (boxer2Weight - boxer1Weight)+("кг"));
+        System.out.println("Разнциа в весе =" + (boxer2Weight % boxer1Weight)+("кг"));
+
+        System.out.println("Задание 8.1");
+        var workHours = 640;
+        var employeeWorks = 8;
+        var totalEmployee = workHours/employeeWorks;
+        System.out.println("Всего работников в компании " + totalEmployee);
+        System.out.println("Задание 8.2");
+        totalEmployee = totalEmployee + 94;
+        workHours = totalEmployee * employeeWorks;
+        System.out.println("Если в компании работает "+ totalEmployee + (" человек, то всего ") + workHours + (" часов работы может быть поделено между сотрудниками"));
     }
 }
